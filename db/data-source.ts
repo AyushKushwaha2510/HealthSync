86f678fa4d38a88c9cb92777a4ce7221d87631a6
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-05-26 20:22:15 +0530

    db config

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,5 +1,9 @@
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Appointment } from "src/appointments/entities/appointment.entity";
+import { Doctor } from "src/doctors/entities/doctor.entity";
+import { Patient } from "src/patients/entities/patient.entity";
+import { User } from "src/users/entities/user.entity";
 
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
     imports: [ConfigModule],
@@ -15,8 +19,8 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
             username: configService.get<string>("dbUsername"),
             database: configService.get<string>("dbName"),
             password: configService.get<string>("dbPassword"),
-            entities: ["dist/**/*.entity.js"],
-            // entities: [User, Artist, Song, Playlist],
+            // entities: ["dist/**/*.entity.js"],
+            entities: [User, Patient, Doctor, Appointment],
             // synchronize: false,
             synchronize: true, // keep it false in production
             migrations: ["dist/db/migrations/*.js"],
