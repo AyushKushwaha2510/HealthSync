b15e2ccc8d2d94bd0bfd5070833b0b6b74e211e4
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-02 13:44:56 +0530

    Doctors Module imported in AdminModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,10 +4,13 @@ import { AdminController } from './admin.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from './entities/admin.entity';
 import { DoctorRegistrationRequestModule } from 'src/doctor-registration-request/doctor-registration-request.module';
+import { DoctorsService } from 'src/doctors/doctors.service';
+import { DoctorsModule } from 'src/doctors/doctors.module';
 
 @Module({
   imports: [
     DoctorRegistrationRequestModule,
+    DoctorsModule,
     TypeOrmModule.forFeature([Admin])
   ],
   controllers: [AdminController],
