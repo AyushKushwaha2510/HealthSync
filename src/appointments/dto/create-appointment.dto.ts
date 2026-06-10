cf48070307108b9bb80f05a664a36f05286a700f
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-10 18:44:27 +0530

    dto for appointment

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,28 @@
-export class CreateAppointmentDto {}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateAppointmentDto {
+  @IsDateString()
+  @IsNotEmpty({ message: 'Select a date and time' })
+  appointmentDateTime!: Date;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+
+  @IsUUID()
+  doctorId!: string;
+
+  @IsOptional()
+  @IsUUID()
+  hospitalId?: string;
+
+  @IsOptional()
+  @IsUUID()
+  clinicId?: string;
+}
