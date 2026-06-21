cfddaa86224cd904ef33d21867d921f9b9b957e1
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-21 16:51:44 +0530

    feat: add AppointmentsModule to PaymentsModule imports

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,13 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
+import { AppointmentsModule } from 'src/appointments/appointments.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [
+    TypeOrmModule.forFeature([Payment]), 
+    AppointmentsModule
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
