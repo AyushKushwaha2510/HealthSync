c67b1b29aa92a608ea835442a951060a7dd307dd
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-01 18:02:04 +0530

    fix: add cookie-parser middleware to the application

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SeedService } from './seed/seed.service';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   // for testing with frontend
   app.enableCors({
