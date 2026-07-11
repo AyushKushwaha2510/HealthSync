5a61b62080061d77ae24990b397161698228a15a
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-11 17:03:17 +0530

    style: remove console log from findOne method in PrescriptionService

--- src/features/prescriptions/prescriptions.service.ts
+++ src/features/prescriptions/prescriptions.service.ts
@@ -125,7 +125,7 @@ export class PrescriptionService {
     });
 
     if (!prescriptions) throw new NotFoundException('Prescription Not Found');
-    console.log('pres', prescriptions);
+    
     return prescriptions;
   }
 
