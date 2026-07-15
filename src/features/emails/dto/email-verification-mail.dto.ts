ff6a5ebd080090c2a03b2192a1390cd9e4427667
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-15 14:39:29 +0530

    feat: add EmailVerificationMailDto for email verification process

---
+++ src/features/emails/dto/email-verification-mail.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class EmailVerificationMailDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName!: string;
+}
