ed26fe453e9b00172987fc955a70647263f0edb5
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-21 17:44:25 +0530

    feat: enhance EmailService to include OTP generation and storage for email verification

--- src/features/emails/emails.service.ts
+++ src/features/emails/emails.service.ts
@@ -1,14 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { Resend } from 'resend';
 import { EmailVerificationMailDto } from './dto/email-verification-mail.dto';
-import { InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import bcrypt from 'bcryptjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Otp } from './entities/opt.entity';
+import { Repository } from 'typeorm';
 
+@Injectable()
 export class EmailService {
   private readonly resendApiKey: string;
   private readonly resend: Resend;
 
   constructor(
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
+
+    @InjectRepository(Otp)
+    private readonly otpRepository: Repository<Otp>,
   ) {
     this.resendApiKey = this.configService.getOrThrow<string>('resendApiKey');
     this.resend = new Resend(this.resendApiKey);
@@ -16,15 +24,28 @@ export class EmailService {
 
   async sendEmailVerificationMail(dto: EmailVerificationMailDto) {
     const otp = Math.ceil(Math.random() * 100000);
+
+    // encode the OTP
+    const hashedOtp = Number(bcrypt.genSalt(otp));
+
+    const newOtp = new Otp();
+    newOtp.email = dto.email;
+    newOtp.otp = hashedOtp;
+
+    // store the otp in DB
+    await this.otpRepository.save(newOtp);
+
     console.log(otp);
     const html = `
       this mail is for verification of your email id 
+      plese enter your otp
+      ${otp}
     `;
-
+    console.log('req, aa hua', dto);
     const { data, error } = await this.resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello World',
+      to: ['ayush.kushwaha23138@gmail.com'],
+      subject: 'OTP confirmations',
       html: '<strong>It works!</strong>',
     });
 
