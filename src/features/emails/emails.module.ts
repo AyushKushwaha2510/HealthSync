15299ba2fbc83e8938b5de51115c47c0743888a9
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-23 13:26:17 +0530

    feat: add TypeOrmModule import for Otp entity integration

--- src/features/emails/emails.module.ts
+++ src/features/emails/emails.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailsController } from './emails.controller';
 import { EmailService } from './emails.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Otp } from './entities/opt.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Otp])],
   controllers: [EmailsController],
   providers: [EmailService],
 })
