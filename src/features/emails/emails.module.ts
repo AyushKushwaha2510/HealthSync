4c99cbb6ac5244ef9b21142bbeb11bdb29e8c7df
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-26 12:53:10 +0530

    feat: add EmailTemplate to providers in EmailsModule

--- src/features/emails/emails.module.ts
+++ src/features/emails/emails.module.ts
@@ -3,11 +3,12 @@ import { EmailsController } from './emails.controller';
 import { EmailService } from './emails.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Otp } from './entities/opt.entity';
+import { EmailTemplate } from './emails.templete';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Otp])],
   controllers: [EmailsController],
-  providers: [EmailService],
+  providers: [EmailService, EmailTemplate],
 })
 
 export class EmailsModule {}
